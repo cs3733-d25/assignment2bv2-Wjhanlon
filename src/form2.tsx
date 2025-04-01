72dbd4ab658669b95ef6a5d2f728ad24cce0b865
Wjhanlon <joeyhanlon44@gmail.com>
2025-03-31 21:20:03 -0400

    Added Hobby2

---
+++ src/form2.tsx
@@ -0,0 +1,57 @@
+export function Form2(){
+   return( <>
+        <h2>Musical Survey Form</h2>
+        <form>
+            <label htmlFor="name">Enter Your Name</label>
+            <input type="text" id="name" placeholder="enter text here"/>
+            <br/>
+            <label htmlFor="email">Enter Your Email</label>
+            <input type="text" id="email" placeholder="enter text here"/>
+            <br/>
+
+            <label>What are your favorite genres?</label>
+            <br/>
+            <label htmlFor="rock"> Rock</label>
+            <input type="checkbox" id="rock"/>
+            <label htmlFor="rap"> Rap</label>
+            <input type="checkbox" id="rap"/>
+            <label htmlFor="pop"> Pop</label>
+            <input type="checkbox" id="pop"/>
+            <label htmlFor="jazz"> Jazz</label>
+            <input type="checkbox" id="jazz"/>
+            <label htmlFor="other"> Other</label>
+            <input type="checkbox" id="other"/>
+            <br/>
+
+            <label>How experienced are you with music production?</label>
+            <br/>
+            <label htmlFor="beginner">Beginner</label>
+            <input type="radio" id="beginner" name="experience" value="beginner"/>
+            <br/>
+            <label htmlFor="intermediate">Intermediate</label>
+            <input type="radio" id="intermediate" name="experience" value="intermediate"/>
+            <br/>
+            <label htmlFor="expert">Expert</label>
+            <input type="radio" id="expert" name="experience" value="expert"/>
+            <br/>
+
+            <label htmlFor="comments">Additional Comments</label>
+            <br/>
+            <textarea id="comments" placeholder="enter text here"></textarea>
+            <br/>
+            <label htmlFor="daw">Choose your favorite DAW (Digital Audio Workspace</label>
+            <select id="daw" name="daw">
+                <option value="Ableton Live">Ableton Live</option>
+                <option value="FL Studio">FL Studio</option>
+                <option value="Reaper">Reaper</option>
+                <option value="Garageband">Garageband</option>
+                <option value="Logic Pro">Logic Pro</option>
+                <option value="Other">Other</option>
+            </select>
+            <br/>
+            <button type="submit">Submit</button>
+        </form>
+
+    </>
+   )
+}
---
+++ src/table2.tsx
@@ -0,0 +1,42 @@
+export function Table2() {
+    return (
+        <>
+        <table className="collapse">
+            <caption>Comparison of Different Music Workspaces</caption>
+            <tbody>
+            <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Ease of Use</th>
+                <th>Overall Rating</th>
+            </tr>
+            </tbody>
+            <tbody>
+            <tr>
+                <th>Ableton Live</th>
+                <th>~$400</th>
+                <th>4/5</th>
+                <th>5/5</th>
+            </tr>
+            </tbody>
+            <tbody>
+            <tr>
+                <th>FL Studio</th>
+                <th>~$200</th>
+                <th>4/5</th>
+                <th>4/5</th>
+            </tr>
+            </tbody>
+            <tbody>
+            <tr>
+                <th>Reaper</th>
+                <th>Free</th>
+                <th>1/5</th>
+                <th>2/5</th>
+            </tr>
+            </tbody>
+        </table>
+            <hr/>
+            </>
+    )
+}
